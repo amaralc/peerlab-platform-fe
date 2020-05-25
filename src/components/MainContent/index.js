7f718a1a28aba21056159a714e725777d4ee32f7
amaralc <amaral.calil@gmail.com>
2020-05-24 21:19:48 -0300

    FEATURE / CONTENT / Create component MainContent

---
+++ src/components/MainContent/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MainContent() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <p>Content Here</p>
+        </div>
+      </div>
+    </>
+  );
+}
