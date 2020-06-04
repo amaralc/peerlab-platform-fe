d65cc3e6694777074888e3135d54ff8e6bf5d78a
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-06-04 10:56:13 -0300

    FEATURE / CONTENT / Muda nomenclatura do componente

--- src/components/CentralCategoriesList/index.js
+++ src/components/CentralCategoriesList/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { Row, Title } from './styles';
 import IconCard from '../IconCard';
 
-export default function ListaCentralCategorias(props) {
+export default function CentralCategoriesList(props) {
   return (
     <Row>
       <Title>{props.title}</Title>
-      <IconCard
-        cardText={props.name}
-        cardImgSrc={props.imgPath}
-        cardRoute="/"
-      />
+
       <IconCard cardText="test" cardImgSrc={props.imgPath} cardRoute="/" />
       <IconCard cardText="test" cardImgSrc={props.imgPath} cardRoute="/" />
       <IconCard cardText="test" cardImgSrc={props.imgPath} cardRoute="/" />
