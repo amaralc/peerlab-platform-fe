d4e59da01066669875edb3f9074f19b1939a8797
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-06-05 13:00:43 -0300

    FEATURE / Cria nova div para testes

---
+++ src/pages/_templates/TemplatePage3/components/Right/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Div } from './styles';
+
+function Right() {
+  return <Div />;
+}
+
+export default Right;
--- src/pages/_templates/TemplatePage3/index.js
+++ src/pages/_templates/TemplatePage3/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import SideBarLeftLocal from './components/SideBarLeftLocal';
 import CentralContentLocal from './components/CentralContentLocal';
+import Right from './components/Right';
 
 export default function TemplatePage3() {
   return (
     <>
       <SideBarLeftLocal />
       <CentralContentLocal title="Faça o pedido" />
+      <Right />
     </>
   );
 }
