50ac495a186b94adb35a4eaec4ad93557e13db70
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-06-04 22:43:49 -0300

    FIX / Limpa componente Find do Navbar

--- src/components/Navbar/Find/styles.js
+++ src/components/Navbar/Find/styles.js
@@ -8,34 +8,3 @@ export const Input = styled.input`
   border-radius: 4px;
   padding-left: 5px;
 `;
-/*
-export const Img = styled.img`
-  width: 40px;
-  margin-left: 4px;
-  color: #fff;
-`;
-
-
-export const Container = styled.div`
-  position: relative;
-  padding: 0 0 0 20px;
-  margin: 0 20px;
-  background: #ddd;
-  direction: rtl;
-  width: 200px;
-`;
-export const Input = styled.input`
-  height: 20px;
-  margin: 0;
-  padding-right: 30px;
-  width: 100%;
-`;
-
-export const Img = styled.img`
-  position: absolute;
-  bottom: 2px;
-  right: 5px;
-  width: 24px;
-  height: 24px;
-`;
-*/
