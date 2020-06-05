81d8426d23643dd6e1bbe82e01cde52c8d06e186
Artur Della Favera <arturdellafavera@gmail.com>
2020-06-05 08:45:57 -0300

    FEATURE / CONTENT / Inverte ordem de card

--- src/components/LabList/index.js
+++ src/components/LabList/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { ListGroup, SideBarItem, SideBarGap } from './styles';
+import { ListGroup, SideBarItem } from './styles';
 
 export default function LabList(props) {
   return (
     <>
       <ListGroup className=" list-group-flush">
         <SideBarItem to="/">
-          <img src={props.imgPath} alt="" />
           <h5>{props.name}</h5>
           <p>{props.text}</p>
+          <img src={props.imgPath} alt="" />
         </SideBarItem>
       </ListGroup>
-      <SideBarGap />
     </>
   );
 }
