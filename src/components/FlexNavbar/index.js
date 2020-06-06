beab66d87d3c6c6ce8b4018a5d685df6572c2756
amaralc <amaral.calil@gmail.com>
2020-06-06 16:44:13 -0300

    FEATURE / STYLE / Add style to button

--- src/components/FlexNavbar/index.js
+++ src/components/FlexNavbar/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import NavItem from './NavItem';
 import Find from './Find';
-import { Container } from './styles';
+import { Container, Brand, Button } from './styles';
 
 export default function NavbarFlex() {
   return (
     <Container>
       <nav className="navbar navbar-expand-lg navbar-dark ">
-        <Link style={{ marginRight: 150 }} className="navbar-brand" to="/">
-          peerLab
-        </Link>
-        <button
-          className="navbar-toggler"
+        <Brand to="/">peerLab</Brand>
+        <Button
           type="button"
           data-toggle="collapse"
           data-target="#navbarResponsive"
           aria-controls="navbarResponsive"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+        />
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <Find />
           <ul className="navbar-nav ml-auto">
--- src/components/FlexNavbar/styles.js
+++ src/components/FlexNavbar/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.nav`
   width: 100%;
@@ -20,5 +21,52 @@ export const Container = styled.nav`
 
   @media (max-width: 300px) {
     min-width: 270px;
+  }
+`;
+
+export const Brand = styled(Link)`
+  display: inline-block;
+  padding-top: 0.3125rem;
+  padding-bottom: 0.3125rem;
+  margin-right: 150px;
+  font-size: 1.25rem;
+  line-height: inherit;
+  white-space: nowrap;
+
+  color: #fff;
+
+  &:focus,
+  :hover {
+    text-decoration: none;
+    color: #fff;
+  }
+`;
+
+export const Button = styled.button`
+  padding: 0.25rem 0.75rem;
+  font-size: 1.25rem;
+  line-height: 1;
+  background-color: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  border-radius: 0.25rem;
+
+  background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke='rgba(255, 255, 255, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: middle;
+  background-size: 100% 100%;
+
+  &:hover,
+  :focus {
+    text-decoration: none;
+  }
+
+  @media (min-width: 992px) {
+    display: none;
+  }
+`;
+
+export const Menu = styled.div`
+  ul {
   }
 `;
