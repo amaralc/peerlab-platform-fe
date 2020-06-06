0aac2fc0d2757f034edb5ae13261e558d1c42ce2
amaralc <amaral.calil@gmail.com>
2020-06-06 14:45:44 -0300

    FIX / STYLE / Remove NavbarContainer and MainContainer

--- src/components/FlexNavbar/styles.js
+++ src/components/FlexNavbar/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const NavbarContainer = styled.div`
+export const Container = styled.nav`
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+
   display: -ms-flexbox;
   display: flex;
   -ms-flex-wrap: wrap;
@@ -9,13 +13,10 @@ export const NavbarContainer = styled.div`
   align-items: center;
   -ms-flex-pack: justify;
   justify-content: space-between;
-`;
 
-export const MainContainer = styled.div`
-  width: 100%;
-  padding: 0rem 0.25rem;
-  margin-right: auto;
-  margin-left: auto;
+  border: solid;
+  border-color: #f33;
+  background-color: #343a40 !important;
 
   @media (max-width: 300px) {
     min-width: 270px;
