79f8258dfb24b4f7762615abf052d6d8419a5669
amaralc <amaral.calil@gmail.com>
2020-06-06 14:15:27 -0300

    FEATURE / STYLE / Add space between

--- src/components/AppWrapper/styles.js
+++ src/components/AppWrapper/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  align-content: space-between;
+  justify-content: space-between;
 
   border: solid;
   border-color: #fa3;
