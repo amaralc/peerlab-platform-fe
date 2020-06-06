e941206d0d3ea3780a0ef02d413b02e0b0d2d3b2
amaralc <amaral.calil@gmail.com>
2020-06-06 17:11:06 -0300

    FEATURE / STYLE / Added styled nav-link

--- src/components/FlexNavbar/NavItem/index.js
+++ src/components/FlexNavbar/NavItem/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from './styles';
 
 export default function NavItem(props) {
   return (
-    <>
-      <li className="nav-item">
-        <Link className="nav-link" to={props.route}>
-          {props.item}
-        </Link>
-      </li>
-    </>
+    <li>
+      <NavLink to={props.route}>{props.item}</NavLink>
+    </li>
   );
 }
---
+++ src/components/FlexNavbar/NavItem/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const NavLink = styled(Link)`
+  display: block;
+  padding: 0.5rem 0.5rem;
+  color: rgba(255, 255, 255, 0.5);
+
+  &:focus,
+  :hover {
+    text-decoration: none;
+    color: rgba(255, 255, 255, 0.75);
+  }
+
+  &:active {
+    color: #fff;
+  }
+`;
