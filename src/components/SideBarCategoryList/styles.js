44ad36e513784080204793334c3d318ce7c6cc5f
amaralc <amaral.calil@gmail.com>
2020-06-07 00:45:24 -0300

    FIX / STYLE / Remove sidebar gap

--- src/components/SideBarCategoryList/styles.js
+++ src/components/SideBarCategoryList/styles.js
@@ -17,11 +17,3 @@ export const SideBarHeading = styled.div`
   font-weight: 500;
   border-bottom: 2px solid #ffd900;
 `;
-
-export const SideBarGap = styled.div`
-  padding: 0.5rem 1.25rem;
-  height: 2rem;
-  color: #495057;
-  font-size: 1rem;
-  font-weight: 500;
-`;
