9c9e3fcbfeba52b25f73c46ca1b51a71df1b7e6f
amaralc <amaral.calil@gmail.com>
2020-06-07 07:14:30 -0300

    FEATURE / CONTENT / Cria e configura homepage (Page00)

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ import TemplatePage00 from './pages/_templates/TemplatePage00';
 import TemplatePage01 from './pages/_templates/TemplatePage01';
 import TemplatePage02 from './pages/_templates/TemplatePage02';
 import TemplatePage03 from './pages/_templates/TemplatePage03';
+import Page00 from './pages/Page00';
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Switch>
-        <Route exact path="/" component={TemplatePage01} />
+        <Route exact path="/" component={Page00} />
         <Route path="/template-page-00" component={TemplatePage00} />
+        <Route path="/template-page-01" component={TemplatePage01} />
         <Route path="/template-page-02" component={TemplatePage02} />
         <Route path="/template-page-03" component={TemplatePage03} />
         <Route path="/wireframe" component={TemplatePageWithWireframe} />
--- src/pages/Page00/index.js
+++ src/pages/Page00/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 // import { Container } from './styles';
-import ContentWrapper from '../../../components/ContentWrapper';
+import ContentWrapper from '../../components/ContentWrapper';
 import ContentLeft from './components/ContentLeft';
 import ContentCenter from './components/ContentCenter';
 
-export default function TemplatePage01() {
+export default function Page00() {
   return (
     <ContentWrapper>
       <ContentLeft />
