4b7e3bf2c33617eddff0626095adbd7233fc0d4d
amaralc <amaral.calil@gmail.com>
2020-06-07 00:47:28 -0300

    FIX / STYLE / Remove app wrapper from app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 
-import AppWrapper from './components/AppWrapper';
 import FlexNavbar from './components/FlexNavbar';
 import FlexFooter from './components/FlexFooter';
 
 export default function App(props) {
   return (
-    <AppWrapper>
+    <>
       <FlexNavbar />
       {props.children}
       <FlexFooter />
-    </AppWrapper>
+    </>
   );
 }
