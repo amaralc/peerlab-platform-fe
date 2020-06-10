126daede74df8918bca18ab1c38994f3bb08ec54
Artur Della Favera <arturdellafavera@gmail.com>
2020-06-10 09:45:10 -0300

    FEATURE / CONTENT / Adiciona conteúdo aos componentes laterais da Page01

---
+++ src/pages/Page01/components/ContentRight/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from './styles';
+
+import SideBarList from '../../../../components/SideBarList';
+import SideBarCard01 from '../../../../components/SideBarCard01';
+
+import MicFluor from '../../img/MicFluor.jpg';
+
+export default function ContentLeft() {
+  return (
+    <Container>
+      <SideBarList title="Template Title 01">
+        <SideBarCard01
+          title="Microscópio de Fluorescência operando novamente!"
+          subtitle="Informamos que o microscópio de fluorescência está operando novamente."
+          imgPath={MicFluor}
+          url="https://lcme.ufsc.br/2020/01/27/microscopio-de-fluorescencia-operando-novamente/"
+        />
+      </SideBarList>
+    </Container>
+  );
+}
