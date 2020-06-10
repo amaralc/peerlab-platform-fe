6288ae1d390f75b90cf6699b490d448df532648b
Artur Della Favera <arturdellafavera@gmail.com>
2020-06-10 09:48:31 -0300

    FEATURE / STYLE / Container passa a ser um styled.a

--- src/components/SideBarCard01/index.js
+++ src/components/SideBarCard01/index.js
@@ -5,7 +5,7 @@ import { Container, Image, InfoContainer, InfoTitle, InfoText } from './styles';
 export default function SideBarCard01(props) {
   return (
     <>
-      <Container to={props.route}>
+      <Container href={props.url}>
         <Image src={props.imgPath} alt="" />
         <InfoContainer>
           <InfoTitle>{props.title}</InfoTitle>
--- src/components/SideBarCard01/styles.js
+++ src/components/SideBarCard01/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const Container = styled(Link)`
+export const Container = styled.a`
   position: relative;
   display: flex;
   flex-direction: column;
