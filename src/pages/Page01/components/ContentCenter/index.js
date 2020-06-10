1e80537d521bcfc4fbfde326083da4ac50c48c9b
Artur Della Favera <arturdellafavera@gmail.com>
2020-06-10 14:36:20 -0300

    FEATURE / CONTENT / Adiciona ícones à Page01-Parte2

--- src/pages/Page01/components/ContentCenter/index.js
+++ src/pages/Page01/components/ContentCenter/index.js
@@ -3,8 +3,6 @@ import { Container, Title, Body } from './styles';
 
 import CentralListOfCategories01 from '../../../../components/CentralListOfCategories01';
 import CentralListOfCategories02 from '../../../../components/CentralListOfCategories02';
-
-import Microscope from '../../img/microscope.png';
 import CentralCard02 from '../../../../components/CentralCard02';
 
 import MicOti1 from '../../../../img/microscopy-nsit0108-01 1.png';
@@ -15,16 +13,17 @@ import MicOti5 from '../../../../img/microscopy-nsit0108-01 8.png';
 import MicElet1 from '../../../../img/microscopy-nsit0108-01 9.png';
 import MicElet2 from '../../../../img/microscopy-yliv-01 8.png';
 import MicElet3 from '../../../../img/microscopy-yliv-01 9.png';
-/** import MicOti1 from '../../../../img/microscopy-yliv-01 8.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 1.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 2.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 3.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 4.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 5.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 6.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 7.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 8.png';
-import MicOti1 from '../../../../img/microscopy-yliv-01 9.png'; */
+/**
+import MicOti6 from '../../../../img/microscopy-yliv-01 1.png';
+import MicOti7 from '../../../../img/microscopy-yliv-01 2.png';
+import MicOti8 from '../../../../img/microscopy-yliv-01 3.png';
+import MicOti9 from '../../../../img/microscopy-yliv-01 4.png';
+import MicOti10 from '../../../../img/microscopy-yliv-01 5.png';
+import MicOti11 from '../../../../img/microscopy-yliv-01 6.png';
+import MicOti12 from '../../../../img/microscopy-yliv-01 7.png';
+import MicOti13 from '../../../../img/microscopy-nsit0108-01 4.png';
+import MicOti14 from '../../../../img/microscopy-nsit0108-01 6.png';
+import MicOti15 from '../../../../img/microscopy-nsit0108-01 7.png'; */
 
 export default function ContentCenter() {
   return (
@@ -68,19 +67,19 @@ export default function ContentCenter() {
         <CentralListOfCategories01 title="Microscópios eletrônicos">
           <CentralListOfCategories02 title="SubCategory 01">
             <CentralCard02
-              title="Microscópio eletrônico"
+              title="Microscópio eletrônico de transmissão"
               imgSrc={MicElet1}
               imgAlt="Microscope"
               route="/template-page-03"
             />
             <CentralCard02
-              title="Microscópio eletrônico"
+              title="Microscópio eletrônico digital"
               imgSrc={MicElet2}
               imgAlt="Microscope"
               route="/template-page-03"
             />
             <CentralCard02
-              title="Microscópio eletrônico"
+              title="Microscópio eletrônico digital"
               imgSrc={MicElet3}
               imgAlt="Microscope"
               route="/template-page-03"
